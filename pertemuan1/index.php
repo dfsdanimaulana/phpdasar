c88394dced3ab1d826d1221e87e6d4676e50f9d5
dani maulana <danimaulana9f@gmail.com>
2021-03-04 21:50:38 +0700

    add sales beta

---
+++ pertemuan1/index.php
@@ -0,0 +1,30 @@
+<?php 
+// standar output
+// echo atua print 
+echo "dani mulana bersahaya";
+echo 123;
+// penulisan sintak php
+// 1. php di dalam html
+// variable dan tie data
+// variable
+// $nama = "dani maulana";
+// echo "haloo nama saya $nama";
+// echo 'haloo nama saya $nama';
+
+// penggabung string "."
+$nama_d = "dani";
+$nama_b = "maulana";
+echo $nama_d." ".$nama_b;
+ ?>
+<!---
+<!DOCTYPE html>
+<html>
+<head>
+	<title>html</title>
+</head>
+<body>
+	<p>hallo selamat datang <?php echo $nama; ?></p>
+	<?php echo "<h1>hai daniiii</h1>"; ?>
+</body>
+</html>
+--->
